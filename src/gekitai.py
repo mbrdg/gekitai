05882252c053213fb2a18223a7dd62b018644495
Miguel Rodrigues <migb.rodrigues@gmail.com>
2022-04-04 20:04:54 +0100

    Initial testing of pygame - it worked! 🎉

--- src/gekitai.py
+++ src/gekitai.py
@@ -1,2 +1,21 @@
+import pygame
+
+
 if __name__ == '__main__':
-    print(f"Hello, world!")
+    pygame.init()
+    pygame.display.set_caption('Gekitai')
+
+    size = width, height = 320, 240
+    screen = pygame.display.set_mode(size)
+
+    rect = pygame.rect.Rect(10, 10, 100, 100)
+    pygame.draw.rect(screen, pygame.color.Color(255, 255, 0), rect)
+
+    running = True
+    while running:
+        pygame.display.update(rect)
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.quit()
