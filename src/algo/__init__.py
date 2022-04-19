465c0f552c22a4e835c93146d192199ed07c2508
Miguel Rodrigues <migb.rodrigues@gmail.com>
2022-04-19 11:57:02 +0100

    evaluators: decoupling evaluators in multiple evaluation functions

--- src/algo/__init__.py
+++ src/algo/__init__.py
@@ -1,4 +1,6 @@
 from .minimax import minimax
 from .mcts import mcts
 
-from .evaluators import board_evaluation
+from .evaluators import markers_evaluator
+from .evaluators import combination_evaluator
+from .evaluators import mix_evaluator
--- src/algo/evaluators.py
+++ src/algo/evaluators.py
@@ -2,26 +2,57 @@ import numpy as np
 from scipy.signal import convolve2d
 
 
-def board_evaluation(game, is_over, is_max):
-    """ Evaluator function for the board """
+def _is_over(is_max):
+    """ If the game is over, returns the score depending on the winner """
+    return np.NINF if is_max else np.PINF
 
-    # If the game is over just return the best value according to the game winner
-    # If the game is over the winner is the previous player
-    if is_over:
-        return np.NINF if is_max else np.PINF
 
+def _markers_evaluator(game, is_max, markers=8):
     me = game.curr_player if is_max else game.prev_player
     opponent = game.prev_player if is_max else game.curr_player
 
-    # Evaluation of the number of markers already placed
     my_placed = np.count_nonzero(game.board == me)
     opponent_placed = np.count_nonzero(game.board == opponent)
-    score = (-8 / (my_placed - 8.01) - 1) - (-8 / (opponent_placed - 8.01) - 1)
 
-    # Evaluation of the number of consecutive markers
+    def f(p, m):
+        return np.inf if p - m == 0 else -m / (p - m) - 1
+
+    return f(my_placed, markers) - f(opponent_placed, markers)
+
+
+def markers_evaluator(game, is_max, is_over, *, markers=8):
+    """ Evaluation based on the number of markers already placed """
+    if is_over:
+        return _is_over(is_max)
+    return _markers_evaluator(game, markers)
+
+
+def _combination_evaluator(game, is_max, weight=10.0):
+    me = game.curr_player if is_max else game.prev_player
+    opponent = game.prev_player if is_max else game.curr_player
+
+    score = 0
     kernels = [np.ones(shape=(1, 2)), np.ones(shape=(2, 1)), np.eye(2), np.fliplr(np.eye(2))]
     for kernel in kernels:
-        score += np.count_nonzero(convolve2d(game.board == me, kernel, mode='valid') == 2) * 10.0
-        score -= np.count_nonzero(convolve2d(game.board == opponent, kernel, mode='valid') == 2) * 10.0
+        score += np.count_nonzero(convolve2d(game.board == me, kernel, mode='valid') == 2) * weight
+        score -= np.count_nonzero(convolve2d(game.board == opponent, kernel, mode='valid') == 2) * weight
 
     return score
+
+
+def combination_evaluator(game, is_max, is_over, *, weight=10.0):
+    """ Evaluation of consecutive markers of the same player """
+    if is_over:
+        return _is_over(is_max)
+    return _combination_evaluator(game, is_max, weight)
+
+
+def mix_evaluator(game, is_max, is_over, *, markers=8, weight=10.0):
+    """ Better evaluation function that combines the markers already placed and their combinations """
+    if is_over:
+        return _is_over(is_max)
+
+    markers_score = _markers_evaluator(game, is_max, markers)
+    combination_score = _combination_evaluator(game, is_max, weight)
+
+    return markers_score + combination_score
